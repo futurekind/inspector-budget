75d2c18950008d7736a767714d8caf87c018db60
futurekind <f.trinler@gmail.com>
2016-12-13 23:38:24 +0100

    Adding basic containers for App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import 'normalize.css/normalize.css';
+
+const View = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+`
+
+const Aside = styled.aside`
+    width: 90px;
+    height: 100%;
+    background: red;
+`
+
+const Main = styled.main`
+    height: 100%;
+    overflow-y: auto;
+`
 
 class App extends Component {
     render () {
         return (
-            <div>
-                <h1>Hall</h1>
-            </div>
+            <View>
+                <Aside></Aside>
+                <Main></Main>
+            </View>
         )
     }
 }

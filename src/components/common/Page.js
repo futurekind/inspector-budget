7015766223bc94c8e79d95681f657504096a2768
Fabian Trinler <fabian.trinler@blateral.com>
2016-12-14 13:48:59 +0100

    Adding a composable Page component

---
+++ src/components/common/Page.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+import { setNavIndex } from '../../redux/actions/application';
+
+const View = styled.section`
+    padding: 24px;
+`;
+
+const mapState = state => {
+    return {}
+}
+
+export const createPageHandler = (Comp, props = {}) => {
+    class Page extends Component {
+
+        componentDidMount() {
+            const { navIndex } = props;
+            const { dispatch } = this.props;
+
+            dispatch(setNavIndex(navIndex))
+        }
+
+        componentWillUnmount () {
+            const { dispatch } = this.props;
+            dispatch(setNavIndex(-1))
+        }
+        
+
+        render () {
+            return (
+                <View>
+                    <Comp {...this.props} />
+                </View>
+            )
+        }
+    }
+
+    return connect(mapState)(Page)
+}

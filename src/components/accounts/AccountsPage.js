0655e9c8d9866d2e7a7ed3bbb505c6736dc88f81
Fabian Trinler <fabian.trinler@blateral.com>
2016-12-27 16:45:05 +0100

    Adjusting layout

--- src/components/accounts/AccountsPage.js
+++ src/components/accounts/AccountsPage.js
@@ -3,13 +3,27 @@ import { connect } from 'react-redux';
 import { createPageHandler } from '../common/Page';
 import styled from 'styled-components';
 
-import PageHeading from '../common/PageHeading'
+import PageHeading from '../common/PageHeading';
+import Divider from '../common/Divider';
 
+const View = styled.section`
+    width: 100%;
+    height: 100%;
+`;
+
+const Header = styled.header`
+    flex-basis: 100%;
+`;
 
 class AccountsPage extends Component {
     render () {
         return (
-            <PageHeading>Accounts</PageHeading>
+            <View>
+                <Header>
+                    <PageHeading>Accounts</PageHeading>
+                    <Divider type="light" />
+                </Header>
+            </View>
         )
     }
 }
--- src/components/common/PageHeading.js
+++ src/components/common/PageHeading.js
@@ -5,7 +5,6 @@ import { colors } from '../../utils/styles'
 const Heading = styled.h1`
     margin: 0;
     padding: 0;
-    padding-bottom: 12px;
     color: ${colors.dark}
     font-weight: 300;
     font-size: 44px;

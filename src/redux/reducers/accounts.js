e01c5800aae695314be8c74757690dd582bd9351
Fabian Trinler <fabian.trinler@blateral.com>
2017-01-04 21:07:10 +0100

    Removing unnessecary break statements

--- src/redux/reducers/accounts.js
+++ src/redux/reducers/accounts.js
@@ -14,19 +14,16 @@ export default (state = initialState, action) => {
             return state
                 .update('results', results => results.push(action.data.id))
                 .update('entities', entities => entities.set(action.data.id, fromJS(action.data)));
-            break;
 
         case types.ACCOUNT__UPDATE:
             return state.update('entities', 
                 entities => entities.mergeIn([action.id], fromJS(action.data)) );
-            break;
 
         case types.ACCOUNT__DELETE:
             const index = state.get('results').findIndex(res => res === action.id);
             return state
                 .deleteIn(['results', index])
                 .deleteIn(['entities', action.id])
-            break;
 
         case types.ACCOUNT__CREATE_DIALOG_OPEN:
             return state.set('createDialogIsOpen', !state.get('createDialogIsOpen'))

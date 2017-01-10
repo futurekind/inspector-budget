b23b49e241d8e3bbf423b325951a1fa657f05125
Fabian Trinler <fabian.trinler@blateral.com>
2017-01-10 15:25:12 +0100

    Adding updateTransaction action creator

--- src/redux/actions/__tests__/transactions.test.js
+++ src/redux/actions/__tests__/transactions.test.js
@@ -1,16 +1,33 @@
 import * as actions from '../transactions';
 
-describe('createTransaction()', () => {
+describe('Transactions actions', () => {
+    
+    describe('createTransaction()', () => {
 
-    it('returns action', () => {
-        expect(actions.createTransaction({
-            foo: 'bar'
-        })).toEqual({
-            type: actions.types.TRANSACTIONS__CREATE,
-            data: {
+        it('returns action', () => {
+            expect(actions.createTransaction({
                 foo: 'bar'
-            }
+            })).toEqual({
+                type: actions.types.TRANSACTIONS__CREATE,
+                data: {
+                    foo: 'bar'
+                }
+            })
         })
+
     })
 
+    describe('updateTransaction()', () => {
+        it('returns action', () => {
+            expect(actions.updateTransaction('abc', {
+                foo: 'baz'
+            })).toEqual({
+                type: actions.types.TRANSACTIONS__UPDATE,
+                id: 'abc',
+                data: {
+                    foo: 'baz'
+                }
+            })
+        })
+    })
 })
--- src/redux/actions/transactions.js
+++ src/redux/actions/transactions.js
@@ -1,8 +1,14 @@
 export const types = {
-    TRANSACTIONS__CREATE: 'TRANSACTIONS__CREATE' 
+    TRANSACTIONS__CREATE: 'TRANSACTIONS__CREATE', 
+    TRANSACTIONS__UPDATE: 'TRANSACTIONS__UPDATE', 
 }
 
 export const createTransaction = data => ({
     type: types.TRANSACTIONS__CREATE,
     data
+})
+
+export const updateTransaction = (id, data) => ({
+    type: types.TRANSACTIONS__UPDATE,
+    id, data
 })

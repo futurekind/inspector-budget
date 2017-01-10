fcbc97e9a8c4a81c9b5b11c9f3d1c73f5c48b3de
Fabian Trinler <fabian.trinler@blateral.com>
2017-01-10 16:21:03 +0100

    Adding CRUD reducer for transactions

--- src/redux/reducers/__tests__/transactions.test.js
+++ src/redux/reducers/__tests__/transactions.test.js
@@ -1,4 +1,5 @@
 import reducer, { initialState } from '../transactions';
+import * as actions from '../../actions/transactions';
 
 describe('Transactions reducer', () => {
 
@@ -7,6 +8,86 @@ describe('Transactions reducer', () => {
     })
 
     it('returns its initial state', () => {
+        const state = reducer(undefined, { type: 'some action' });
+        expect(state).toBe(initialState)
+    })
+
+    describe('handles TRANSACTIONS__CREATE', () => {
+        let state;
+
+        beforeEach(() => {
+            state = reducer(undefined, actions.createTransaction({
+                foo: 'bar'
+            }))
+        })
+
+        it('adds to results field', () => {
+            expect(state.get('results').size).toBe(1)
+        })
+
+        it('adds to entities field', () => {
+            const entities = state.get('entities');
+            
+            expect(entities.size).toBe(1)
+            expect(entities.first().get('foo')).toBe('bar')
+        })
+
+    })
+
+    describe('handles TRANSACTIONS__UPDATE', () => {
+        let state;
+        let transaction;
+        let id;
+
+        beforeEach(() => {
+            state = reducer(undefined, actions.createTransaction({
+                foo: 'bar'
+            }))
+            transaction = state.getIn(['entities', state.getIn(['results', 0])])
+            id = transaction.get('id');
+        })
+
+        it('overwrites data', () => {
+            state = reducer(state, actions.updateTransaction(id, {
+                foo: 'baz'
+            }))
+
+            expect(state.getIn(['entities', id]).get('foo')).toBe('baz')
+        })
+
+        it('adds new data', () => {
+            state = reducer(state, actions.updateTransaction(id, {
+                newKey: 'some data'
+            }))
+
+            expect(state.getIn(['entities', id]).get('newKey')).toBe('some data')
+        })
+
+    })
+
+    describe('handles TRANSACTIONS__DELETE', () => {
+        let state;
+        let transaction;
+        let id;
+
+        beforeEach(() => {
+            state = reducer(undefined, actions.createTransaction({
+                foo: 'bar'
+            }))
+            transaction = state.getIn(['entities', state.getIn(['results', 0])])
+            id = transaction.get('id');
+        })
+
+        it('deletes from results field', () => {
+            state = reducer(state, actions.deleteTransaction(id))
+
+            expect(state.get('results').size).toBe(0)
+        })
+
+        it('deletes from entities field', () => {
+            state = reducer(state, actions.deleteTransaction(id))
+            expect(state.get('entities').size).toBe(0)
+        })
 
     })
 
--- src/redux/reducers/transactions.js
+++ src/redux/reducers/transactions.js
@@ -1,1 +1,30 @@
-export default (state, action) => state;
+import { Map, List, fromJS } from 'immutable';
+import { types } from '../actions/transactions';
+
+export const initialState = Map({
+    results: List(),
+    entities: Map()
+})
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        
+        case types.TRANSACTIONS__CREATE:
+            return state
+                    .update('results', results => results.push(action.data.id))
+                    .update('entities', entities => entities.set(action.data.id, fromJS(action.data)))
+
+        case types.TRANSACTIONS__UPDATE:
+            return state.update('entities', 
+                entities => entities.mergeIn([action.id], fromJS(action.data)) );
+
+        case types.TRANSACTIONS__DELETE:
+            const index = state.get('results').findIndex(result => result === action.id);
+            return state
+                .deleteIn(['results', index])
+                .deleteIn(['entities', action.id])
+
+        default:
+            return state;
+    }
+};

45f4146f21d84203490863074fde5355fb4df317
Fabian Trinler <fabian.trinler@blateral.com>
2017-01-10 14:07:07 +0100

    wip transactions reducer

---
+++ src/redux/reducers/__tests__/transactions.test.js
@@ -0,0 +1,13 @@
+import reducer, { initialState } from '../transactions';
+
+describe('Transactions reducer', () => {
+
+    it('is a function', () => {
+        expect(typeof reducer).toBe('function')
+    })
+
+    it('returns its initial state', () => {
+
+    })
+
+})

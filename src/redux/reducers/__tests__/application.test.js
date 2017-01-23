3e229e5c6dc35b6c4d6da61341f7f3fdcdebcfe6
futurekind <f.trinler@gmail.com>
2017-01-23 22:05:10 +0100

    Adding reducer for APP__SET_DIRTY

--- src/redux/reducers/__tests__/application.test.js
+++ src/redux/reducers/__tests__/application.test.js
@@ -2,13 +2,14 @@ import reducer, { initialState } from '../application';
 import * as fromActions from '../../actions/application';
 
 describe('Application Reducer', () => {
+    let state;
 
     it('is defined', () => {
         expect(typeof reducer).toBe('function');
     })
 
     it('returns the initialState', () => {
-        const state = reducer(undefined, {
+        state = reducer(undefined, {
             type: 'TEST'
         });
         
@@ -16,9 +17,14 @@ describe('Application Reducer', () => {
     })
 
     it('responds to APP__SET_NAV_INDEX', () => {
-        const state = reducer(undefined, fromActions.setNavIndex(1))
+        state = reducer(undefined, fromActions.setNavIndex(1))
 
         expect(state.get('selectedNavIndex')).toBe(1)
+    })
+
+    it('handles APP__SET_DIRTY', () => {
+        state = reducer(undefined, fromActions.setDirty())
+        expect(state.get('dirty')).toBe(true)
     })
 
 })
